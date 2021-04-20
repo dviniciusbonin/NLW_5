d5ad7c5aa2c23da607f58df2c1083cfca3f26246
DVinciusBonin <vinibonin007@gmail.com>
2021-04-19 23:54:28 -0300

    create first route get

--- src/server.ts
+++ src/server.ts
@@ -2,6 +2,10 @@ import express from 'express'
 
 const app = express()
 
+app.get('/', (request, response) => {
+    return response.status(200).json({message: "Hello World"})
+})
+
 app.listen(3000, () => {
     console.log('Server is running on port 3000')
 })

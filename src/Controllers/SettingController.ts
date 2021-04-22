5cc6663eb4a0f09bf56898c25777a1dd7a7bc34a
DVinciusBonin <vinibonin007@gmail.com>
2021-04-21 23:10:25 -0300

    initial config services

--- src/Controllers/SettingController.ts
+++ src/Controllers/SettingController.ts
@@ -1,22 +1,23 @@
 import {Request , Response} from 'express'
-import { getCustomRepository } from 'typeorm'
-import { SettingsRepository } from '../Repositories/SettingsRepository'
+import { SettingService } from '../Services/SettingService'
 
 class SettingController {
 
     async create(request: Request, response: Response) {
 
         const {chat , username} = request.body
-        const settingsRepository = getCustomRepository(SettingsRepository)
-    
-        const settings = settingsRepository.create({
-            chat,
-            username
-        })
-    
-        await settingsRepository.save(settings)
-    
-        return response.json(settings)
+        
+        const settingService = new SettingService()
+
+        try {
+            const settings = await settingService.create({chat, username})
+        
+            return response.json(settings)
+        }catch(error) {
+            return response.status(400).json({
+                message: error.message
+            })
+        }
 
     } 
 
